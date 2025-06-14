97b4b52fcfac0aade98349735638b8262637ac50
Bahadur Ali <bahadurali123470@gmail.com>
2025-06-14 17:35:26 +0500

    Refactor image hover functionality and clean up code

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-// const btn = document.querySelector("#throttle");
 const throttleFunction = (func, delay)=> {
     let prev = 0;
     return (...args) => {
@@ -11,30 +8,44 @@ const throttleFunction = (func, delay)=> {
         }
     }
 }
+
+let imgUrls = [
+    "https://images.unsplash.com/photo-1582785514150-015b4c70a84a?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1745484880060-49d13176e483?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1739649913570-cb8c1b2f6f57?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1722109178543-0c967dc93d3b?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1749410347863-3aa1b4dbd027?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1559743520-7c95478709fc?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1566982160502-5dc607d396de?w=900&auto=format&fit=crop&q=60",
+    "https://images.unsplash.com/photo-1649328087396-13bb77cb279a?w=900&auto=format&fit=crop&q=60"
+];
+
 document.querySelector("#center").addEventListener("mousemove", throttleFunction((dets)=>{
     let div = document.createElement("div");
     div.classList.add("imgHover");
-    div.style.left = dets.clientX+"px"
-    div.style.top = dets.clientY+"px"
+    div.style.left = dets.clientX + "px";
+    div.style.top = dets.clientY + "px";
 
     let img = document.createElement("img");
-    img.setAttribute("src", "https://images.unsplash.com/photo-1603700584787-4f331a469c7a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNsYXNzaWNhbHxlbnwwfDF8MHx8fDA%3D")
-    div.appendChild(img);
+    let randomImg = Math.floor(Math.random() * imgUrls.length);
+    img.src = imgUrls[randomImg];
 
-    document.body.appendChild(div)
+    div.appendChild(img);
+    document.body.appendChild(div);
 
     gsap.to(img, {
         y: "0",
         ease: Power1,
         duration: 0.3
-    })
+    });
+
     gsap.to(img, {
         y: "100%",
         ease: Power3,
-        delay: 0.5,
-    })
+        delay: 0.5
+    });
 
     setTimeout(() => {
         div.remove();
-    }, 1000)
+    }, 1000);
 }, 200));
